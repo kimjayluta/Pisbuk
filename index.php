2e33b7766f083163bd84a6a1c246549032d76000
kimjayluta <kimjayluta0721@gmail.com>
2018-05-09 15:51:35 +0800

    added codes in index.php file and fix the cookies

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ function isLoggedIn(){
     if (isset($_COOKIE['SNID'])){
         if (DB::query('SELECT user_id FROM login_tokens WHERE token=:token',array(':token'=>sha1($_COOKIE['SNID'])))){
             $userid = DB::query('SELECT user_id FROM login_tokens WHERE token=:token',array(':token'=>sha1($_COOKIE['SNID'])))[0]['user_id'];
-
+            
             if (isset($_COOKIE['SNID_'])){
                 return $userid;
             } else {
