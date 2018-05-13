e15cefd540417c66193199dbbeff4a360210a325
kimjayluta <kimjayluta0721@gmail.com>
2018-05-13 13:15:40 +0800

    updated sql

--- socialnetwork.sql
+++ socialnetwork.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2018 at 08:25 AM
+-- Generation Time: May 13, 2018 at 07:14 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -39,8 +39,19 @@ CREATE TABLE `login_tokens` (
 --
 
 INSERT INTO `login_tokens` (`id`, `token`, `user_id`) VALUES
-(1, '5a6da0f69dca2c5bc74e5e24dfed1aa826b87dc1', 1),
-(2, 'e5cf5c322d1f66fa6da4ef61e8eef3d0cf6d1a44', 1);
+(4, 'aae1d3c8d32f2a1075aac4954fdf54b299b0bec3', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_tokens`
+--
+
+CREATE TABLE `password_tokens` (
+  `id` int(11) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -60,7 +71,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'password', '$2y$10$ddc5q2dyqUJVFXtLSLETWei6wxkft3e1dsSmk2PwPYNvHoZOpeFh2', 'luislovekate@gmail.com');
+(1, 'password', '$2y$10$M4Xq9zFiomOM9oQqtcekw.n3Jho7StcBqaMDx5WOnEjYZGPhbgIKa', 'luislovekate@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -73,6 +84,13 @@ ALTER TABLE `login_tokens`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `token` (`token`),
   ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `password_tokens`
+--
+ALTER TABLE `password_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`);
 
 --
 -- Indexes for table `users`
@@ -88,7 +106,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `login_tokens`
 --
 ALTER TABLE `login_tokens`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `password_tokens`
+--
+ALTER TABLE `password_tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
