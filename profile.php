6a5736528e455ea090766ab8faa598316c72fad1
kimjayluta <kimjayluta0721@gmail.com>
2018-05-14 18:19:17 +0800

    Added follow function and Updated sql file

---
+++ profile.php
@@ -0,0 +1,31 @@
+<?php
+include('./classes/db.php');
+include('./classes/log_in.php');
+
+$username = "";
+
+if (isset($_GET['username'])){
+    if (DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))){
+
+        $username = DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['username'];
+
+        if (isset($_POST['follow'])){
+            $userid = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
+            $followerid = login::isLoggedIn();
+            
+            if (!DB::query('SELECT followers_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))){
+                DB::query('INSERT INTO followers VALUES(\'\',:userid,:followerid)', array(':userid'=>$userid,':followerid'=>$followerid));
+            } else {
+                echo 'Already following';
+                exit();
+            }
+        }
+    } else {
+        die('User not found!');
+    }
+}
+?>
+<h1><?php echo $username;?>'s Profile</h1>
+<form action="profile.php?username=<?php echo $username; ?>" method="post">
+    <input type="submit" name="follow" value="Follow">
+</form>
